d192a0f72f0c9336c443a1b9573f4d0d78275b02
ikura1 <ikura.lab@gmail.com>
2018-06-09 20:41:58 +0900

    refactor: connect4のコードを見て書き換え

--- gomoku.py
+++ gomoku.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 
 class Gomoku(object):
@@ -7,107 +8,129 @@ class Gomoku(object):
     """
     def __init__(self):
         # 盤の初期化
-        self.ban = [[0 for _ in range(9)]
-                    for _ in range(9)]
+        self.current_player = 1
+        self.stats = GomokuStats(np.zeros(81, dtype=np.int),
+                                 self.current_player)
         self.history_dict = {1: [],
                              2: []}
+
+
+    def reset(self):
+        """
+        ゲームをリセットします
+        :return:
+        """
         self.current_player = 1
+        self.stats = GomokuStats(np.zeros(81, dtype=np.int),
+                                 self.current_player)
 
-    def check_renzoku(self, x, y):
-        def check_renzoku_row(i):
-            count = 0
-            length = len(self.ban) - x if i == 1 else x + 1
-            cx = x + i
-            for _ in range(length):
-                if self.ban[cx][y] != value:
-                    break
-                
-            return count
-        def check_renzoku_col(i):
-            length = len(self.ban) - y if i == 1 else y + 1
-            count = 0
-            return count
-        def check_renzoku_cross(i):
-            x, y
-            count = 0
-            return count
-        value = self.ban[x][y]
-
-        for i in range():
-
-
-    def do_coma(self, x, y):
+    def turn(self, position):
         """
-        駒を置く
-
-        Parameters
-        ----------
-        x: int
-        y: int
-
-        Returns
-        -------
-        win_player: int
+        TODO: 場外のqエラー対応 (99, -1など)
+        TODO: 既にコマがある場合対応
+        :param position:
+        :return:
         """
-        self.ban[x][y] = self.current_player
-        self.history_dict[self.current_player].append([x, y])
-        if self.judg_winner() == -1:
-            self.current_player = 2 if self.current_player == 1 else 1
-        return self.judg_winner()
-
-    def check_win(self):
-        """
-        勝負が終ったかの判定
-        Returns
-        -------
-        exit_flg: bool
-        """
-        # x, y = self.history_dict[self.current_player]
-        exit_flg = False
-        return exit_flg
-
-    def judg_winner(self):
-        """
-        勝者は返す
-
-        Returns
-        -------
-
-        """
-        if self.check_win():
-            return self.current_player
-        return -1
-
-    def view_ban(self):
-        """
-        盤の表示
-        Returns
-        -------
-
-        """
-        # print(self.ban)
-        # return
-        print('-' * 37)
-        for row in self.ban:
-            print('|', end='')
-            for col in row:
-                print('', col, '|', end='')
-                # print('|', end='')
-            print('')
-            print('-' * 37)
+        self.stats, done = self.stats.take_action(position)
+        self.current_player = -self.current_player
+        return done
 
     def run(self):
         """
         ゲームの開始
         """
-        player_input_text = '{c_player}(例: 0,2): '
+        player_input_text = '{c_player}(例: 1): '
+        self.stats.render()
         while True:
-            hoge = input(player_input_text.format(c_player=self.current_player))
-            winner = self.do_coma(*[int(i) for i in hoge.split(',')])
-            self.view_ban()
-            if winner != -1:
+            position = input(player_input_text.format(c_player=self.current_player))
+            done = self.turn(int(position))
+            self.stats.render()
+            if done:
+                print('WINNER: {0}'.format(-self.current_player))
                 break
 
-if __name__ == u'__main__':
-    test = gomoku()
+
+class GomokuStats(object):
+    """
+    ゲームの状況
+    """
+    def __init__(self, board, player, position=0):
+        self.board = board
+        self.player = player
+        self.is_end = self._check_end_game(position)
+
+    def _check_end_game(self, position):
+        """
+        ゲームが終了したか確認します
+        :param position: 置いた駒の位置
+        :return: ゲームが終了したかどうか
+        """
+        win_patterns = self._create_win_patterns(position)
+        for pattern in win_patterns:
+            if sum(self.board[pattern]) == 5 * -self.player:
+                return 1
+        return 0
+
+    def _create_win_patterns(self, position):
+        """
+        置いた駒を含む勝利パターンを返します
+        :param position: 置いた駒の位置
+        :return: 勝利パターンのリスト
+        """
+        sposition = position // 9 * 9
+        lines = [[sposition + i for i in range(9)]]
+        angles = [8, 9, 10]
+        lines.extend([self._get_line_numbers(position, angle)
+                      for angle in angles])
+        patterns = [line[i: i + 5]
+                    for line in lines
+                    for i in range(len(line))
+                    if i < 5]
+        return filter(lambda x: position in x, patterns)
+
+    def _get_line_numbers(self, position, angle):
+        """
+        指定したラインの数字リストを返します
+        :param position: 取得するラインの起点
+        :param angle: 取得するラインの方向
+        :return: ラインの数字リスト
+        """
+        i = position % angle
+        numbers = []
+        while i < len(self.board):
+            numbers.append(i)
+            i += angle
+        return numbers
+
+    def take_action(self, position):
+        """
+        ターンのアクション
+        :param position: 駒を置いた位置
+        :return: 次の盤面、ゲームが終了したか
+        """
+        new_board = np.array(self.board)
+        new_board[position] = self.player
+        new_stats = GomokuStats(new_board, -self.player, position)
+        done = new_stats.is_end
+        return new_stats, done
+
+    def render(self):
+        """
+        ゲーム盤を表示します
+        """
+        # print(self.board)
+        # return
+        print('-' * 46)
+        for i in range(9):
+            row = self.board[i * 9: (i + 1) * 9]
+            print('|', end=' ')
+            for col in row:
+                print('{0:02d}'.format(col), '| ', end='')
+                # print('|', end='')
+            print('')
+            print('-' * 46)
+
+
+if __name__ == '__main__':
+    test = Gomoku()
     test.run()
